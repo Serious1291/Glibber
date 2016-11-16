f44155be297c160b27f5651a50d473a9a7ab972a
Serious1291 <sascha.meyer7@gmx.de>
2016-11-16 18:31:35 +0100

    first change

---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+    public int rotationSpeed;
+    public string name;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rotation = transform.position;
+	}
+    public Vector3 rotation;
+    public float timer;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += 0.01f * rotationSpeed;
+        transform.rotation = Quaternion.Euler(timer, timer / 2f, timer / 4f);
+	}
+}
