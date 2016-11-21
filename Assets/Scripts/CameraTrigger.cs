6804e3dd13531bc61c8c91f4fc61217b3ee3aae9
Silenciano <arno.claus@hotmail.de>
2016-11-21 15:11:07 +0100

    added camera

--- Assets/Scripts/CameraTrigger.cs
+++ Assets/Scripts/CameraTrigger.cs
@@ -5,18 +5,82 @@ using UnityEngine.SceneManagement;
 public class CameraTrigger : MonoBehaviour
 {
 
-    // used for look around
-    private float horizontalSpeed = 2.0f;
-    private float verticalSpeed = 2.0f;
+    public static Transform cameraObject;
+
+    // used for look around with mouse2
     private float y = 0.0f;
     private float x = 0.0f;
+
+    private static float maxX = 4.5f;
+    private static float minX = -4.5f;
+    private static float maxY = 5f;
+    private static float minY = -4.5f;
+
+    private static float maxZ = 5f;
+    private static float minZ = -5f;
+
+
+    // change fieldOfView with mouse Wheel
+    private int zoomSpeed = 10;
+    private float minFOV = 35f;
+    private float maxFOV = 80f;
+
+    // speed for camera movements at keybindings
+    private int cameraSpeed = 100;
 
     void Start()
     {
         // important that the camera is not reseted 
         x = 26;
-        y= -24.6f;
+        y = -24.6f;
         Camera.main.transform.rotation = Quaternion.Euler(x, y, 0.0f);
+    }
+
+
+    private Vector3 setX(Vector3 tmpVector, float value)
+    {
+
+        tmpVector.x += value;
+        if (tmpVector.x < minX)
+        {
+            tmpVector.x = minX;
+        }
+        else if (tmpVector.x > maxX)
+        {
+            tmpVector.x = maxX;
+        }
+        return tmpVector;
+
+    }
+
+    private Vector3 setY(Vector3 tmpVector, float value)
+    {
+
+        tmpVector.y += value;
+        if (tmpVector.y < minY)
+        {
+            tmpVector.y = minY;
+        }
+        else if (tmpVector.y > maxY)
+        {
+            tmpVector.y = maxY;
+        }
+        return tmpVector;
+    }
+
+    private Vector3 setZ(Vector3 tmpVector, float value)
+    {
+
+        tmpVector.z += value;
+        if (tmpVector.z < minZ)
+        {
+            tmpVector.z = minZ;
+        }
+        else if (tmpVector.z > maxZ)
+        {
+            tmpVector.z = maxZ;
+        }
+        return tmpVector;
     }
 
     void Update()
@@ -28,54 +92,78 @@ public class CameraTrigger : MonoBehaviour
             SceneManager.LoadScene(2);
         }
 
-        int zoomChange = 0;
+        if (Input.GetButton("Fire2"))
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
 
-        if (Input.GetButton("Fire2")) { 
-            y += horizontalSpeed * Input.GetAxis("Mouse X");
-            x -= verticalSpeed * Input.GetAxis("Mouse Y");
+            Vector3 tmpVector = Camera.main.transform.position;
+            tmpVector = setX(tmpVector, mouseX);
+            tmpVector = setY(tmpVector, mouseY);
+
+            Camera.main.transform.position = tmpVector;
+        }
+
+        if (Input.GetButton("Fire2"))
+        {
+            y += 2.0f * Input.GetAxis("Mouse X");
+            x -= 2.0f * Input.GetAxis("Mouse Y");
 
             Camera.main.transform.rotation = Quaternion.Euler(x, y, 0.0f);
+
         }
 
-        // zoom 
-        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        // fov
+
+        // zoom in
+        float delta = Input.GetAxis("Mouse ScrollWheel") * -zoomSpeed;
+        if (delta < 0)
         {
-            zoomChange += 1;
-            transform.Translate(Vector3.forward * zoomChange);
-        } else if(Input.GetAxis("Mouse ScrollWheel") < 0)
+            // make sure the current FOV is within min values
+            if (Camera.main.fieldOfView + delta > minFOV-1)
+            {
+                Camera.main.fieldOfView = Camera.main.fieldOfView + delta;
+            }
+        }
+        // zoom out
+        else if (delta > 0)
         {
-            zoomChange -= 1;
-            transform.Translate(Vector3.forward * zoomChange);
+            // make sure the current FOV is within max values
+            if (Camera.main.fieldOfView + delta < maxFOV+1)
+            {
+                Camera.main.fieldOfView = Camera.main.fieldOfView + delta;
+            }
         }
 
+
+        // define way around cube:
+        
 
         // move left and right and top and bot
         // this is not smooth if the camera is moved around 30-40°
-        
+
         if (Input.GetKey(KeyCode.A))
         {
-            Vector3 tmpVector = Camera.main.transform.position;
-            tmpVector.x -= 0.4f;
-            Camera.main.transform.position = tmpVector;
+            transform.RotateAround(cameraObject.transform.position, Vector3.up, cameraSpeed * Time.deltaTime);
+            //transform.Translate(Vector3.right * -Time.deltaTime * zoomSpeed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Vector3 tmpVector = Camera.main.transform.position;
-            tmpVector.x += 0.4f;
-            Camera.main.transform.position = tmpVector;
+            transform.RotateAround(cameraObject.transform.position, Vector3.down, cameraSpeed * Time.deltaTime);
+            //transform.Translate(Vector3.right * Time.deltaTime * zoomSpeed);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            Vector3 tmpVector = Camera.main.transform.position;
-            tmpVector.z += 0.4f;
-            Camera.main.transform.position = tmpVector;
+            transform.RotateAround(cameraObject.transform.position, Vector3.right, cameraSpeed * Time.deltaTime);
+            //transform.Translate(Vector3.up * Time.deltaTime * zoomSpeed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            Vector3 tmpVector = Camera.main.transform.position;
-            tmpVector.z -= 0.4f;
-            Camera.main.transform.position = tmpVector;
+            transform.RotateAround(cameraObject.transform.position, Vector3.left, cameraSpeed * Time.deltaTime);
+            //transform.Translate(Vector3.down * Time.deltaTime * zoomSpeed);
         }
+
+        Camera.main.transform.LookAt(cameraObject);
 
     }
 
