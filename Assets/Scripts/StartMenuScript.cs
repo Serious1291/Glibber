8a10fc55065af1d297339d87a8f6987103a17d17
Serious1291 <sascha.meyer7@gmx.de>
2016-11-22 15:21:55 +0100

    MainMenu Scenes are working now

--- Assets/Scripts/StartMenuScript.cs
+++ Assets/Scripts/StartMenuScript.cs
@@ -5,11 +5,15 @@ public class StartMenuScript : MonoBehaviour
 {
 
     public Canvas MainCanvas;
+    public Canvas SettingsCanvas;
+    public Canvas ControlsCanvas;
 
     // starts before the start-Function
     void Awake()
     {
         MainCanvas.enabled = true;
+        SettingsCanvas.enabled = false;
+        ControlsCanvas.enabled = false;
     }
 
     public void ExitGameButton()
@@ -25,9 +29,27 @@ public class StartMenuScript : MonoBehaviour
 
     public void Settings()
     {
-        MainCanvas.enabled = false; 
+        MainCanvas.enabled = false;
+        SettingsCanvas.enabled = true; 
     }
 
+    public void ReturnToMenuButton()
+    {
+        SettingsCanvas.enabled = false;
+        MainCanvas.enabled = true;
+    }
+
+    public void ControlsButton()
+    {
+        SettingsCanvas.enabled = false;
+        ControlsCanvas.enabled = true;
+    }
+
+    public void ReturnToSettingsButton()
+    {
+        ControlsCanvas.enabled = false;
+        SettingsCanvas.enabled = true;
+    }
 
     //later
 
